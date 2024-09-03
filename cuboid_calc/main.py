ac4b9b61429a8fe94777ef7c979c4d727cd99772
Nelson Malbone <nhmalbone@icloud.com>
2024-09-03 17:21:32 -0400

    09/03/2024 to found the cuboid volume if given length width and height.

--- cuboid_calc/main.py
+++ cuboid_calc/main.py
@@ -1,3 +1,11 @@
 # found the volume of a cuboid
 # l * w * h = volume units
 
+length = float(input('Enter a length: '))
+width = float(input('Enter a width: '))
+height = float(input('Enter a height: '))
+
+volume = length * width * height
+
+print(f'You enter the length of {length} and the width of {width} and the'
+      f'  height of {height} which will give you the cuboid volume of {volume} units')
