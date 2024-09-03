bfa9a46ba704a6fac545a1b6544ec461d37fd93a
Nelson Malbone <nhmalbone@icloud.com>
2024-09-03 15:41:01 -0400

    09/03/2024 this project is simple, have users input a widt and a height to found an area for a rectangle

--- rectangle_area.py
+++ rectangle_area.py
@@ -1,3 +1,12 @@
 # finding the area of a rectangle
 # have user input the width
 # have user input the height
+
+# have users input a width and height
+width = float(input("Enter a Width: "))
+height = float(input("Enter a height: "))
+
+area = width * height
+
+print(f'You enter the width of {width} and the height of {height} which will give'
+      f' you the area of {area}')
